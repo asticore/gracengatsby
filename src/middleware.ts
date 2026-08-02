e1d8c60f940465cbe0df99d112c1e42b9943e31b
Asticore <20883398+asticore@users.noreply.github.com>
2026-08-02 11:46:19 +1000

    Add middleware to append x-pathname header

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next()
+  response.headers.append('x-pathname', request.nextUrl.pathname)
+  return response
+}
